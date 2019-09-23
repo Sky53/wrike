4fb96718d4759d239c6ed3cc4a81adaf141f7186
Sky53 <sanyaga3@yandex.ru>
2019-09-23 19:45:13 +0400

    add test

---
+++ src/test/java/ru/sky/test/wrike/WebDriverSettings.java
@@ -0,0 +1,24 @@
+package ru.sky.test.wrike;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebDriverSettings {
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\1\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
+}
--- src/test/java/ru/sky/test/wrike/WrikeTest.java
+++ src/test/java/ru/sky/test/wrike/WrikeTest.java
@@ -2,16 +2,74 @@ package ru.sky.test.wrike;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.sky.test.wrike.utils.GeneratorEmail;
 
-public class WrikeTest {
+import java.util.List;
+import java.util.Random;
+
+public class WrikeTest extends WebDriverSettings {
+
+
     @Test
-    public void wrikeTest() {
-        System.setProperty("webdriver.chrome.driver","C:\\1\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
+    public void wrikeTestOpen() {
         driver.get("https://www.wrike.com/");
         String title = driver.getTitle();
-        Assert.assertTrue(title.equals("Your online project management software - Wrike"));
-        driver.quit();
+        Assert.assertEquals("Your online project management software - Wrike", title);
+
+    }
+
+    @Test
+    public void wrikeTest() throws InterruptedException {
+        driver.get("https://www.wrike.com/");
+
+        WebElement webElement = driver.findElements(By.cssSelector(".wg-header__free-trial-button.wg-btn.wg-btn--green")).get(2);
+        webElement.click();
+        String email = GeneratorEmail.getTestEmail((int) (Math.random() * 10));
+
+        driver.findElement(By.cssSelector(".wg-input.modal-form-trial__input")).sendKeys(email);
+        driver.findElement(By.cssSelector((".wg-btn.wg-btn--blue.modal-form-trial__submit"))).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+
+        wait.until(ExpectedConditions.urlToBe("https://www.wrike.com/resend/"));
+        Assert.assertEquals("https://www.wrike.com/resend/", driver.getCurrentUrl());
+
+        Random random = new Random();
+
+        int firstAnswer = random.nextInt(2);
+        int secondAnswer = 2 + random.nextInt(5);
+        int thirdAnswer = 7 + random.nextInt(3);
+
+        List<WebElement> answers = driver.findElements(By.cssSelector(".switch__button"));
+        answers.get(firstAnswer).click();
+        answers.get(secondAnswer).click();
+        answers.get(thirdAnswer).click();
+
+
+        if (thirdAnswer == 9) {
+            driver.findElement(By.cssSelector(".switch__input")).sendKeys("test");
+        }
+
+
+        driver.findElement(By.cssSelector(".submit.wg-btn.wg-btn--navy.js-survey-submit")).click();
+
+
+
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElements(By.cssSelector(".switch__button")).get(0)));
+        Assert.assertFalse(driver.findElements(By.cssSelector(".switch__button")).get(0).isDisplayed());
+
+
+        WebElement twitterButton = driver.findElement(By.cssSelector(".wg-footer__group.wg-footer__group--social")).findElements(By.cssSelector(".wg-footer__social-link")).get(0);
+        Assert.assertTrue(twitterButton.isDisplayed());
+
+        Assert.assertEquals("https://twitter.com/wrike", twitterButton.getAttribute("href"));
+
+
+        String imageAddress = twitterButton.findElement(By.tagName("use")).getAttribute("xlink:href");
+        Assert.assertEquals("/content/themes/wrike/dist/img/sprite/vector/footer-icons.symbol.svg?v2#twitter", imageAddress);
     }
 }
